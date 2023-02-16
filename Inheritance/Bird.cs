db2341f93b5edaa05d5583f7fc115b21b5124fad
Brian Shaw <1BrianShaw@gmail.com>
2023-02-16 14:48:53 -0500

    exercise 1

---
+++ Inheritance/Bird.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Bird : Animal
+    {
+        public int FlockSize { get; set; }
+
+        public string From { get; set; }
+
+        public bool CanFly { get; set; }
+
+        public bool HasFeathers { get; set; }
+
+        public Bird()
+        {
+
+        }
+        public Bird(string name, int age, int legs, bool hasBackbone, bool hasFeathers, bool canFly, int flockSize, string from)
+        {
+            Name = name;
+            Age = age;
+            Legs = legs;
+            HasBackbone= hasBackbone;
+            HasFeathers= hasFeathers;
+            FlockSize = flockSize;
+            From = from;
+        }
+    }
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.NetworkInformation;
+using System.Xml.Linq;
 
 namespace Inheritance
 {
@@ -28,12 +30,52 @@ namespace Inheritance
              *  
              * Creatively display the class member values 
              */
+            Bird Ostrich = new Bird()
+            {
+                Name = "Ostrich",
+                Age = 30,
+                Legs = 2,
+                HasBackbone = true,
+                HasFeathers = true,
+                CanFly = false,
+                FlockSize = 10,
+                From = "African Savanah",
+            };
+            Console.WriteLine($"--------------Information on the {Ostrich.Name}------------------------");
+            Console.WriteLine();
+            Console.WriteLine($"The {Ostrich.Name} lives in the {Ostrich.From}.");
+            Console.WriteLine($"They have {Ostrich.Legs} poweful legs and commonly live in groups of {Ostrich.FlockSize} and can reach {Ostrich.Age} years old.");
+            Console.WriteLine($"It is {Ostrich.HasBackbone} it is a vertibre; it is {Ostrich.HasFeathers} it has feathers;  But unlike other birds it is {Ostrich.CanFly} that it can fly.");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
 
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Reptile class
              *  
              * Creatively display the class member values 
              */
+            Reptile Croc = new Reptile()
+            {
+                Name = "Crocodile",
+                Age = 60,
+                Legs = 4,
+                HasBackbone = true,
+                IsColdBlooded = true,
+                HasScales = true,
+                Environment = "Freshwater and Saltwater",
+                Defense = "bite",
+            };
+
+            Console.WriteLine($"--------------Information on the {Croc.Name}------------------------");
+            Console.WriteLine();
+            Console.WriteLine($"The {Croc.Name} has {Croc.Legs}, lives in {Croc.Environment} swams, inlets, and rivers, and has {Croc.Legs}");
+            Console.WriteLine($"It is {Croc.HasBackbone} that they are vertibrates; It is {Croc.IsColdBlooded} that it is Cold blooded; It is {Croc.HasScales} that it has scales.");
+            Console.WriteLine($"If you see a {Croc.Name} it could be as much as {Croc.Age} years old.");
+
         }
+
+
+
     }
 }
